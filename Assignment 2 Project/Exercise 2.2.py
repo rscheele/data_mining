51a240de5da6e0a3af1c38c35c54df2506e0ee41
DESKTOP-E92O7LU\Rodi <rooddei@hotmail.com>
2017-10-08 17:19:41 +0200

    homework 2 almost done

--- Assignment 2 Project/Exercise 2.2.py
+++ Assignment 2 Project/Exercise 2.2.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 from scipy.io import loadmat
 from numpy import reshape
-from sklearn.preprocessing import StandardScaler
+from itertools import combinations
 
 # Load Matlab data file to python dict structure
 mat_data = loadmat('./Data/zipdata.mat')
@@ -15,29 +16,48 @@ traindata = traindata[traindata[:,0]<=1., :]
 X = traindata[:,1:]
 y = traindata[:,0]
 
-# Standardize and normalize data
-X = StandardScaler().fit_transform(X)
-X = (X - np.mean(X)) / np.std(X)
-
-cov_mat = np.cov(X.T)
-eig_vals, eig_vecs = np.linalg.eig(cov_mat)
-
-# U = eig vecs of cov matrix
-# s = singular values of the data matrix  are equal to the square roots of the eigenvalues of the covariance matrix
-# V = Principal components
-U,s,Vt = np.linalg.svd(X.T)
-V = Vt.T
-
-#Z = X*V[:,:4]
-
-#print Z
-
-# Visualize the i'th digit as an image
-''''for i in range (1,11):
+for i in range (1,11):
     plt.subplot(3,4,i);
     I = reshape(X[i,:],(16,16))
     plt.imshow(I, extent=(0,16,0,16), cmap=cm.gray_r);
     title = str("Digit " + str(i) + " as an image")
     plt.title(title);
 plt.subplots_adjust(hspace=.7)
-plt.show()'''''
+plt.show()
+
+# Normalize data
+u = np.mean(X)
+X = np.asmatrix(X - u)
+
+U, s, V = np.linalg.svd(X,full_matrices=False)
+
+Xpca = np.array(X * V[:, :4])
+X = Xpca * V[:, :4].T + u
+
+# Visualize the i'th digit as an image
+for i in range (1,11):
+    plt.subplot(3,4,i);
+    I = reshape(X[i,:],(16,16))
+    plt.imshow(I, extent=(0,16,0,16), cmap=cm.gray_r);
+    title = str("Digit " + str(i) + " as an image")
+    plt.title(title);
+plt.subplots_adjust(hspace=.7)
+plt.show()
+
+# PCA combinations
+k = 1
+for i,j in list(combinations([0,1,2,3], 2)):
+    plt.subplot(2, 3, k);
+    k = k+1
+    plt.scatter(Xpca[:,i],Xpca[:,j],norm=True,s=2)
+    plt.title(str('PC' + str(i) + ' to PC' + str(j)));
+    plt.ylabel(str('PC' + str(j)))
+    plt.xlabel(str('PC' + str(i)))
+plt.subplots_adjust(hspace=.7)
+plt.show()
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection = '3d')
+ax.scatter(Xpca[:,0], Xpca[:,1], Xpca[:,2], c='blue')
+plt.show()
--- Assignment 2 Project/Exercise 2.3.py
+++ Assignment 2 Project/Exercise 2.3.py
@@ -1,6 +1,34 @@
 import numpy as np
+from itertools import combinations
 
 limbs = [2,3,6,8,11,18]
 print 'Mean is ' , np.mean(limbs)
 print 'Standard deviation is ' , np.std(limbs).tostring()
 
+array_n2 = []
+array_n4 = []
+
+print 'Combinations for two aliens are'
+for i,j in list(combinations(limbs, 2)):
+    print str(str(i) + ' and '  + str(j))
+    mean = (i+j)/2.
+    array_n2.append(mean)
+    print str('With mean ' + str(mean))
+
+print 'Combinations for four aliens are'
+for i,j,k,l in list(combinations(limbs, 4)):
+    print str(str(i) + ','  + str(j) + ','+ str(k) + ','  + str(l))
+    mean = (i+j+k+l)/4.
+    array_n4.append(mean)
+    print str('With mean ' + str(mean))
+
+mean_n2 = np.mean(array_n2)
+mean_n4 = np.mean(array_n4)
+
+print 'Mean for N2 is ' , str(mean_n2)
+print 'Mean for N4 is ' , str(mean_n4)
+
+print np.std(array_n2)
+print np.std(array_n4)
+print np.sqrt(2)*np.sqrt(((6-2)*(6-1)))
+print np.sqrt(4)*np.sqrt(((6-4)*(6-1)))
