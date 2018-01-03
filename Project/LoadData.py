d365868279da5d7630ec570c76f36092de1a309b
Rodi-PC\Rodi <rooddei>
2018-01-03 20:58:49 +0100

    project grouped

--- Project/LoadData.py
+++ Project/LoadData.py
@@ -1,6 +1,8 @@
 import numpy as np
-from datetime import datetime
+import pandas as pd
 
-'DateTime, CustomerID, Product Subclass, ProductID'
-D01 = np.loadtxt('Data/D01.txt', dtype=[('f0',datetime),('f1',int),('f4',int),('f5',long)], delimiter=';', skiprows=1, usecols=(0,1,4,5))
-print D01
+'''DateTime, CustomerID, Product Subclass, ProductID'''
+D01 = pd.read_csv('Data/D01.txt', sep=';', header=0, names=['DateTime','CustomerID','ProductSubclass', 'ProductID'], usecols=(0,1,4,5))
+groupedData = np.asmatrix(D01.groupby(['DateTime','CustomerID']))
+
+print np.asmatrix(groupedData)
