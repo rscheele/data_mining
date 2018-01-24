77251e67ee4c5a5a80ec08ea5bb1e5a1eb405ec8
Rodi-PC\Rodi <rooddei>
2018-01-24 21:12:45 +0100

    improved algorithm almost finito

--- Project/improvedapriorimining.py
+++ Project/improvedapriorimining.py
@@ -10,9 +10,9 @@ def generate_association_rules(filename, support, confidence, maxr):
     """1) Compute frequent 1-itemset"""
     # L1 = All frequent 1 itemsets > Support
     # D = All transactions
-    L1, D, C1 = frequent_1_itemsets(filename, support)
-    print(len(L1))
-    freq_itemsets(L1, D, support, C1)
+    L1, D, transactions = frequent_1_itemsets(filename, support)
+
+    freq_itemsets(L1, support)
     return 0
 
 ''' 1) Frequent 1 itemsets '''
@@ -51,99 +51,32 @@ def frequent_1_itemsets(filename, support):
         print('Item ID=' + str(L1[i][0]) + ' Supp=' + str(L1[i][2]))
     print("-----------------------------------------------------------------")'''''
 
-    return (L1, D, C1)
+    return (L1, D, transactions)
 
-def freq_itemsets(L1, D, support, C1):
+def freq_itemsets(L1, support):
     k = 2
-    Lk_1 = []
-    Ck = []
-    for item in range(0, len(L1)):
-        Lk_1.append((L1[item][0],L1[item][2]))
-    C2 = list(itertools.combinations(Lk_1, k))
-    for i in range(0,len(C2)):
-        print(min(C2[i][:][1]))
-
-
-def frequent_itemsets(L1, D, support, C1):
-    k = 2
-    Lk_1 = []
+    support = 10
     Lk = []
-    L = []
-    L.append(L1)
-    count = 0
-    transactions = 0
-    for item in range(0, len(L1)):
-        Lk_1.append(L1[item][0])
-    while Lk_1 != []:
-        Ck = []
-        Lk = []
-        print('calculating candidates...')
-        Ck = candidate_itemsets(Lk_1, k - 1)
-        print('done!')
-        for i in range(0,len(Ck)):
-            C1 = []
-            print(Ck[i])
-            for j in range(0,len(L1)):
-                print(L1[j][0])
-        ''''for c in Ck:
-            count = 0
-            transactions = 0
-            s = set(c)
-            for T in D:
-                transactions += 1
-                t = set(T)
-                if s.issubset(t) == True:
-                    count += 1
-            if (100 * count / transactions) >= support:
-                c.sort()
-                Lk.append((c, ('sup=', round(100 * count / transactions, 2))))
-        Lk_1 = []'''''
-        Lk_1 = []
-        if (len(Lk) > 0):
-            print("-------TOP 10 (or less) FREQUENT %d-ITEMSET------------------------" % k)
-            print(*['set= {{ {} }},  {} {}'.format(', '.join(item[0]), item[1][0], item[1][1]) for item in
-                    sorted(Lk, key=lambda item: item[1][1], reverse=True)][:10], sep='\n')
-            print("------------------------------------------------------------------")
-        for l in Lk:
-            Lk_1.append(l[0])
+
+    while True:
+        Ck = list(itertools.combinations(L1, k))
+        L1 = []
+
+        for i in range(0, len(Ck)):
+            transactionlist = []
+            idList = []
+            s = 0
+            for j in range(0, len(Ck[i])):
+                transactionlist.append(Ck[i][j][1])
+            s = len(set(transactionlist[0]).intersection(*transactionlist[1:]))
+            if (s >= support):
+                Lk.append((idList,s))
+                for m in range(0, len(Ck[i])):
+                    if Ck[i][m] not in L1:
+                        L1.append(Ck[i][m])
+        if L1 == []:
+            break
         k += 1
-        if Lk != []:
-            L.append(Lk)
 
-    return L
-
-"""candidate_itemsets function to compute candidate k-itemset, (Ck) , using frequent (k-1)-itemset, (Lk_1)"""
-def candidate_itemsets(Lk_1, k):
-    length = k
-    Ck = []
-    for list1 in Lk_1:
-        for list2 in Lk_1:
-            count = 0
-            c = []
-            if list1 != list2:
-                while count < length-1:
-                    if list1[count] != list2[count]:
-                        break
-                    else:
-                        count += 1
-                else:
-                    if list1[length-1] < list2[length-1]:
-                        for item in list1:
-                            c.append(item)
-                        c.append(list2[length-1])
-                        if not has_infrequent_subset(c, Lk_1, k):
-                            Ck.append(c)
-                            c = []
-    return Ck
-
-"""has_infrequent_subsets function to determine if pruning is required to remove unfruitful candidates (c) using the Apriori property, with prior knowledge of frequent (k-1)-itemset (Lk_1)"""
-def has_infrequent_subset(c, L1, k):
-    list = set(itertools.combinations(c,k))
-    for item in list:
-        s = []
-        for l in item:
-            s.append(l)
-        s.sort()
-        if s not in L1:
-            return True
-    return False
+    for i in range(0,len(Lk)):
+        print(Lk[i])
